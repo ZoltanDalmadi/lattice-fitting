32ae5f3b6682ca58bf93f249b24ecce5c3b03de2
Zoltan Dalmadi <dmz985@gmail.com>
2016-02-22 21:13:00 +0100

    Utilize the widget for parameters

--- src/PointDetector.cpp
+++ src/PointDetector.cpp
@@ -2,20 +2,29 @@
 #include "PointDetector.hpp"
 
 PointDetector::PointDetector
-(const cv::Mat& image, const cv::Mat& image_gray, QObject* parent)
+(const cv::Mat& image, const cv::Mat& image_gray,
+ PointDetectorWidget& widget, QObject* parent)
   : QObject(parent),
     _image(image),
-    _image_gray(image_gray) {}
+    _image_gray(image_gray),
+    _widget(widget) {}
 
 void PointDetector::detect()
 {
   _points_image = _image.clone();
 
   cv::goodFeaturesToTrack(
-    _image_gray, _points, _max_corners, _qualityLevel, _minDistance, cv::Mat(),
-    _blockSize, _useHarrisDetector, _k
+    _image_gray, _points,
+    _widget._max_corners->value(),
+    _widget._qualityLevel->value(),
+    _widget._minDistance->value(),
+    cv::Mat(),
+    _widget._blockSize->value(),
+    false,
+    0.04
   );
 
   for (const auto& point : _points)
-    cv::circle(_points_image, point, _pointRadius, cv::Scalar(0, 0, 255), -1, 8, 0);
+    cv::circle(_points_image, point, _widget._pointRadius->value(),
+               cv::Scalar(0, 0, 255), -1, 8, 0);
 }
--- src/PointDetector.hpp
+++ src/PointDetector.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <QObject>
 #include <opencv2/opencv.hpp>
+#include "PointDetectorWidget.hpp"
 
 class PointDetector : public QObject
 {
@@ -8,36 +9,18 @@ class PointDetector : public QObject
 
 public:
   PointDetector
-  (const cv::Mat& image, const cv::Mat& image_gray, QObject* parent = 0);
+  (const cv::Mat& image, const cv::Mat& image_gray,
+   PointDetectorWidget& widget, QObject* parent = 0);
 
 public slots:
   void detect();
-  void setMaxCorners(size_t value);
-  //  void setQualityLevel(double value);
-  //  void setMinDistance(double value);
-  //  void setBlockSize(size_t value);
-  //  void setPointRadius(int value);
-  //  void useHarrisDetector(bool value);
-  //  void setK(double value);
 
 private:
   friend class LatticeFittingWindow;
 
   const cv::Mat& _image;
   const cv::Mat& _image_gray;
+  PointDetectorWidget& _widget;
   cv::Mat _points_image;
   std::vector<cv::Point2f> _points;
-
-  size_t _max_corners = 23;
-  double _qualityLevel = 0.01;
-  double _minDistance = 10;
-  size_t _blockSize = 3;
-  int _pointRadius = 4;
-  bool _useHarrisDetector = false;
-  double _k = 0.04;
 };
-
-inline void PointDetector::setMaxCorners(size_t value)
-{
-  _max_corners = value;
-}
